4c22be6d76236155c9fde46ee3d9ad6ac1f974b8
tanv08 <tk362@njit.edu>
2021-12-16 05:35:16 -0500

    Updated files

--- calc/calculations/subtraction.py
+++ calc/calculations/subtraction.py
@@ -9,9 +9,10 @@ from calc.calculations.calculation import Calculation
 class Subtraction(Calculation):
     """ Performs subtraction between two values coming from Parent Class and gives the results """
 
-    def getresult(self):
+    def get_result(self):
         """ Using self to reference the data contained in the object instance """
         subtraction_of_values = self.values[0]
         for value in self.values[1:]:
             subtraction_of_values = subtraction_of_values - value
         return round(subtraction_of_values, 3)
+
