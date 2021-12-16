0299c429eeaf2e9c305c2eb97595259217aa69ea
tanv08 <tk362@njit.edu>
2021-12-16 03:55:46 -0500

    Updated files

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from app.controllers.index_controller import IndexController
 from app.controllers.calculator_controller import CalculatorController
 from app.controllers.Pylint_controller import PylintController
+from app.controllers.AAA_controller import AAAController
+from app.controllers.Article1_controller import Article1Controller
+from app.controllers.Article2_controller import Article2Controller
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -22,4 +25,18 @@ def calculator_post():
 @app.route("/Pylint", methods=['GET'])
 def pylint_get():
     return PylintController.get()
+
+@app.route("/AAA", methods=['GET'])
+def pylint_get():
+    return AAAController.get()
+
+@app.route("/Article1", methods=['GET'])
+def pylint_get():
+    return Article1Controller.get()
+
+@app.route("/Article2", methods=['GET'])
+def pylint_get():
+    return Article2Controller.get()
+
+
 
