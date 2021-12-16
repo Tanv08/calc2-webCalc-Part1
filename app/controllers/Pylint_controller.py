fbff47c83b57ba71de149df4b81cee260817f7d8
tanv08 <tk362@njit.edu>
2021-12-16 02:05:32 -0500

    Updated controllers

--- app/controllers/Pylint_controller.py
+++ app/controllers/Pylint_controller.py
@@ -5,4 +5,4 @@ from flask import render_template
 class Article1Controller(ControllerBase):
     @staticmethod
     def get():
-        return render_template('p2_article1.html')
+        return render_template('Pylint.html')
